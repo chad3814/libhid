fc1d2316553504667f67967a8e668188f4114eb5
Martin F. Krafft <libhid@pobox.madduck.net>
2004-01-26 18:08:18 +0000

    implemented rrudimentary get/set exchange methods. sending works, getting unknown. probably needs lots of testing.

--- src/hid_exchange.c
+++ src/hid_exchange.c
@@ -16,6 +16,8 @@ hid_return hid_get_input_report(HIDInterface* const hidif, int const path[],
   ASSERT(hid_is_opened(hidif));
   ASSERT(buffer);
 
+  if (!buffer) return HID_RET_INVALID_PARAMETER;
+
   if (!hid_is_opened(hidif)) {
     WARNING("the device has not been opened.");
     return HID_RET_NOT_OPENED;
@@ -30,33 +32,81 @@ hid_return hid_get_input_report(HIDInterface* const hidif, int const path[],
   int len = usb_control_msg(hidif->dev_handle,
       USB_ENDPOINT_IN + USB_TYPE_CLASS + USB_RECIP_INTERFACE,
       HID_REPORT_GET,
-      hidif->hid_data->ReportID + (HID_RT_INPUT << 8),
+      hidif->hid_data->ReportID + (HID_RT_OUTPUT << 8),
       hidif->interface,
       buffer, size, USB_TIMEOUT);
 
-    
+  if (len < 0) {
+    WARNING("failed to retrieve report from " TRACEDEVICESTR ".", TRACEDEVICEARGS);
+    return HID_RET_FAIL_GET_REPORT;
+  }
 
-//     if ((replen=libusb_get_report(hData.ReportID, raw_buf, 10)) > 0) {
-        /* Extract the data value */
-  //      GetValue((const unsigned char *) raw_buf, &hData);
+  if (len != size) {
+    WARNING("failed to retrieve complete report to " TRACEDEVICESTR
+        "; requested: %d bytes, got: %d bytes.", TRACEDEVICEARGS, 
+        size, len);
+    return HID_RET_FAIL_GET_REPORT;
+  }
 
+  NOTICE("successfully retrieved report from " TRACEDEVICESTR ".", TRACEDEVICEARGS);
   return HID_RET_SUCCESS;
 }
 
 hid_return hid_set_output_report(HIDInterface* const hidif, int const path[],
     unsigned int const depth, char const* const buffer, unsigned int const size)
 {
+  ASSERT(hid_is_initialised());
+  ASSERT(hid_is_opened(hidif));
+  ASSERT(buffer);
+
+  if (!buffer) return HID_RET_INVALID_PARAMETER;
+
+  if (!hid_is_opened(hidif)) {
+    WARNING("the device has not been opened.");
+    return HID_RET_NOT_OPENED;
+  }
+
+  TRACE("sending report to " TRACEDEVICESTR "...", TRACEDEVICEARGS);
+  hidif->hid_data->Type = ITEM_OUTPUT;
+  hidif->hid_data->ReportID = 0;
+
+  hid_find_object(hidif, path, depth);
+
+  int len = usb_control_msg(hidif->dev_handle,
+      USB_ENDPOINT_OUT + USB_TYPE_CLASS + USB_RECIP_INTERFACE,
+      HID_REPORT_SET,
+      hidif->hid_data->ReportID + (HID_RT_OUTPUT << 8),
+      hidif->interface,
+      buffer, size, USB_TIMEOUT);
+
+  if (len < 0) {
+    WARNING("failed to send report to " TRACEDEVICESTR ".", TRACEDEVICEARGS);
+    return HID_RET_FAIL_SET_REPORT;
+  }
+
+  if (len != size) {
+    WARNING("failed to send complete report to " TRACEDEVICESTR
+        "; requested: %d bytes, sent: %d bytes.", TRACEDEVICEARGS, 
+        size, len);
+    return HID_RET_FAIL_SET_REPORT;
+  }
+
+  NOTICE("successfully sent report to " TRACEDEVICESTR ".", TRACEDEVICEARGS);
   return HID_RET_SUCCESS;
 }
 
 hid_return hid_get_item_string(HIDInterface* const hidif, int const path[],
     unsigned int const depth, char *const value, unsigned int const maxlen)
 {
+  bool const not_yet_implemented = false;
+  ASSERT(not_yet_implemented);
   return HID_RET_SUCCESS;
 }
 
 hid_return hid_set_item_value(HIDInterface* const hidif, int const path[],
     unsigned int const depth, double const value)
 {
+  bool const not_yet_implemented = false;
+  ASSERT(not_yet_implemented);
   return HID_RET_SUCCESS;
 }
