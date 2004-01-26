8815b235df5d8e811db1cae0b8827fefba00305d
Martin F. Krafft <libhid@pobox.madduck.net>
2004-01-26 14:50:45 +0000

    included debug.h from assert.h because of reference to debug_* variables.

--- include/assert.h
+++ include/assert.h
@@ -5,6 +5,8 @@
 #  error "this file is only supposed to be used from within libhid."
 #endif // HID_INTERNAL
 
+#include <debug.h>
+
 #define ASSERT(a) if (!a && hid_debug_stream && hid_debug_level & HID_DEBUG_ASSERTS) \
   fprintf(hid_debug_stream, "*** ASSERTION FAILURE in %s() [%s:%d]: %s\n", \
       __FUNCTION__, __FILE__, __LINE__, #a)
