c1203fc91fc4bf91eabf77bca80007e4b9874978
Arnaud Quette <aquette.dev@gmail.com>
2004-02-09 15:31:37 +0000

    alpha version of hid_get_item_value() function

--- src/hid_exchange.c
+++ src/hid_exchange.c
@@ -95,6 +95,52 @@ hid_return hid_set_output_report(HIDInterface* const hidif, int const path[],
   return HID_RET_SUCCESS;
 }
 
+hid_return hid_get_item_value(HIDInterface* const hidif, int const path[],
+			      unsigned int const depth, double *const value)
+{
+  ASSERT(hid_is_initialised());
+  ASSERT(hid_is_opened(hidif));
+
+  int size;
+  unsigned char buffer[32]; /* FIXME: dyn alloc or argument */
+
+  if (!hid_is_opened(hidif)) {
+    WARNING("the device has not been opened.");
+    return HID_RET_NOT_OPENED;
+  }
+
+  TRACE("retrieving report from " TRACEDEVICESTR "...", TRACEDEVICEARGS);
+  hidif->hid_data->Type = ITEM_FEATURE;
+  hidif->hid_data->ReportID = 0;
+
+  hid_find_object(hidif, path, depth);
+  size = *GetReportOffset(hidif->hid_parser,
+			  hidif->hid_data->ReportID,
+			  hidif->hid_data->Type);
+
+  int len = usb_control_msg(hidif->dev_handle,
+      USB_ENDPOINT_IN + USB_TYPE_CLASS + USB_RECIP_INTERFACE,
+      HID_REPORT_GET,
+      hidif->hid_data->ReportID + (HID_RT_FEATURE << 8),
+      hidif->interface,
+      buffer, size, USB_TIMEOUT);
+
+  if (len < 0) {
+    WARNING("failed to retrieve report from " TRACEDEVICESTR ".", TRACEDEVICEARGS);
+    return HID_RET_FAIL_GET_REPORT;
+  }
+
+  if (len != size) {
+    WARNING("failed to retrieve complete report to " TRACEDEVICESTR
+        "; requested: %d bytes, got: %d bytes.", TRACEDEVICEARGS, 
+        size, len);
+    return HID_RET_FAIL_GET_REPORT;
+  }
+
+  NOTICE("successfully retrieved report from " TRACEDEVICESTR ".", TRACEDEVICEARGS);
+  return HID_RET_SUCCESS;
+}
+
 hid_return hid_get_item_string(HIDInterface* const hidif, int const path[],
     unsigned int const depth, char *const value, unsigned int const maxlen)
 {
