68e52c0bf758bc1794aa60ed5e51751521c6178c
Charles Lepple <clepple+libhid@ghz.cc>
2007-01-20 18:17:18 +0000

    Update to hidwrap.py from Andreas

--- swig/hidwrap.py
+++ swig/hidwrap.py
@@ -13,10 +13,11 @@ for identifier in dir(hid):
 
 # error handling --------------------------------------------------------------
 class HIDError(Exception):
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, code, description):
+        self.code = code
+        self.description = description
     def __str__(self):
-        return repr(self.value)
+        return repr((self.code, self.description))
 
 
 
@@ -44,19 +45,22 @@ def _hid_raise(op, result):
             HID_RET_OUT_OF_SPACE: "Ran out of space",
             HID_RET_FAIL_SET_REPORT: "Failed to set report",
             HID_RET_FAIL_GET_REPORT: "Failed to get report",
-            HID_RET_FAIL_INT_READ: "Failed to read int (?)",
+            HID_RET_FAIL_INT_READ: "Failed to read interrupt report",
             HID_RET_NOT_FOUND: "Not found"
             }
 
     if isinstance(result, int):
         result_code = result
         retval = None
-    elif isinstance(result, tuple):
+    elif isinstance(result, (tuple, list)):
         result_code = result[0]
-        if len(tuple) == 2:
-            retval = tuple[1]
+        if len(result) == 2:
+            retval = result[1]
         else:
-            retval = tuple[1:]
+            retval = result[1:]
+    else:
+        raise ValueError, "result must be either an int or a tuple"
+
     if result_code != hid.HID_RET_SUCCESS:
         try:
             raise HIDError, (result_code, "%s: %s" % (op, code_strings[result_code]))
@@ -107,7 +111,8 @@ def _finalize_hid():
 
 # interface -------------------------------------------------------------------
 class Interface:
-    def __init__(self, vendor_id, product_id, force_open=True, retries=3):
+    def __init__(self, vendor_id, product_id, interface_number, 
+            force_open=True, retries=3):
         self.is_open = False
 
         if not is_initialized():
@@ -121,7 +126,7 @@ class Interface:
 
         if force_open:
             _hid_raise("force_open", hid.hid_force_open(
-                self.interface, 0, matcher, retries))
+                self.interface, interface_number, matcher, retries))
         else:
             _hid_raise("open", hid.hid_force_open(self.interface, 0, matcher))
 
@@ -144,26 +149,34 @@ class Interface:
         _hid_raise("close", hid.hid_close(self.interface))
 
     def get_input_report(self, path, size):
-        print path
-        return _hid_raise(hid.hid_get_input_report(self.interface, path, size))
+        return _hid_raise("get_input_report",
+                hid.hid_get_input_report(self.interface, path, size))
 
     def set_output_report(self, path, bytes):
-        _hid_raise(hid.hid_set_output_report(self.interface, path, bytes))
+        _hid_raise("set_output_report",
+                hid.hid_set_output_report(self.interface, path, bytes))
 
     def get_feature_report(self, path, size):
-        return _hid_raise(hid.hid_get_feature_report(self.interface, path, size))
+        return _hid_raise("get_feature_report",
+                hid.hid_get_feature_report(self.interface, path, size))
 
     def set_feature_report(self, path, bytes):
-        _hid_raise(hid.hid_set_feature_report(self.interface, path, bytes))
+        _hid_raise("set_feature_report",
+                hid.hid_set_feature_report(self.interface, path, bytes))
 
     def get_item_value(self, path):
-        return _hid_raise(hid.hid_get_item_value(self.interface, path))
+        return _hid_raise("get_item_value",
+                hid.hid_get_item_value(self.interface, path))
 
     def interrupt_read(self, endpoint, size, timeout):
-        return _hid_raise(hid.hid_interrupt_read(self.interface, endpoint, size, timeout))
+        return _hid_raise("interrupt_read",
+                hid.hid_interrupt_read(self.interface, endpoint, size, timeout))
 
     def interrupt_write(self, endpoint, bytes, timeout):
-        return _hid_raise(hid.hid_interrupt_write(self.interface, endpoint, bytes, timeout))
+        return _hid_raise("interrupt_write",
+                hid.hid_interrupt_write(self.interface, endpoint, bytes, timeout))
 
     def set_idle(self, duration, report_id):
-        return _hid_raise(hid.hid_set_idle(self.interface, duration, report_id))
+        return _hid_raise("set_idle",
+                hid.hid_set_idle(self.interface, duration, report_id))
+
