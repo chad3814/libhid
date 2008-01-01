2a4b86ce8fe44295dedb34ad097d741334f7be9e
Charles Lepple <clepple+libhid@ghz.cc>
2008-01-01 00:02:46 +0000

    Added some comments and default parameters to swig/hidwrap.py.

--- swig/hidwrap.py
+++ swig/hidwrap.py
@@ -1,7 +1,10 @@
 import hid
 
+'''This module provides an interface to libhid that should be more familiar to
+Python programmers.
 
-
+See the test_hidwrap.py script for an example of how to use this module.
+'''
 
 # import all symbolic constants from hid --------------------------------------
 for identifier in dir(hid):
@@ -13,6 +16,10 @@ for identifier in dir(hid):
 
 # error handling --------------------------------------------------------------
 class HIDError(Exception):
+    '''The exception class for errors raised by hidwrap.
+    ``code`` is the return code (HID_RET_*) and ``description`` is an error
+    string from hid.hid_strerror().
+    '''
     def __init__(self, code, description):
         self.code = code
         self.description = description
@@ -23,6 +30,10 @@ class HIDError(Exception):
 
 
 def _hid_raise(op, result):
+    '''Called on return from a libhid function. It interprets the result code,
+    and raises a HIDError exception if the function returned a non-zero error
+    code.
+    '''
     if isinstance(result, int):
         result_code = result
         retval = None
@@ -48,12 +59,25 @@ def _hid_raise(op, result):
 
 # debugging -------------------------------------------------------------------
 def set_debug(value):
+    '''Set the debug level to the bitwise OR of one or more of the following:
+     * HID_DEBUG_NONE (0; default)
+     * HID_DEBUG_ERRORS 
+     * HID_DEBUG_WARNINGS
+     * HID_DEBUG_NOTICES
+     * HID_DEBUG_TRACES
+     * HID_DEBUG_ASSERTS
+     * HID_DEBUG_NOTRACES
+     * HID_DEBUG_ALL
+    '''
     hid.hid_set_debug(value)
 
 def set_debug_stream(stream):
+    '''Send debug messages to a specific Python output stream (such as
+    sys.stdout or sys.stderr.'''
     hid.hid_set_debug_stream(stream)
 
 def set_usb_debug(level):
+    '''Set the debug level used by libusb.'''
     hid.hid_set_usb_debug(level)
 
 
@@ -63,6 +87,8 @@ def set_usb_debug(level):
 IS_INITIALIZED = [False]
 
 def init():
+    '''Calls hid_init(), and registers an atexit procedure to clean up at
+    termination time.'''
     import atexit
 
     _hid_raise("init", hid.hid_init())
@@ -85,7 +111,15 @@ def _finalize_hid():
 
 # interface -------------------------------------------------------------------
 class Interface:
-    def __init__(self, vendor_id, product_id, interface_number, 
+    '''A HID Interface object.
+
+    Constructing an interface calls hid_force_open() (or hid_open, if force = False).
+
+    Usage: iface = Interface(vendor_id = 0x1234, product_id = 0x5678)
+    
+    Raises a HIDError exception if the interface cannot be opened.
+    '''
+    def __init__(self, vendor_id, product_id, interface_number = 0, 
             force=True, retries=3):
         self.is_open = False
 
@@ -108,18 +142,22 @@ class Interface:
 
 
     def write_identification(self, stream):
+        '''Print the vendor, model and serial number strings, as available.'''
         _hid_raise("write_identification", hid.hid_write_identification(
             stream, self.interface))
 
     def dump_tree(self, stream):
+        '''Dump the HID tree to a Python output stream.'''
         _hid_raise("dump_tree", hid.hid_dump_tree(
             stream, self.interface))
 
     def __del__(self):
+        '''If the interface has been properly opened, close it.'''
         if self.is_open:
             self.close()
 
     def close(self):
+        '''Close the Interface. Called automatically by __del__().'''
         _hid_raise("close", hid.hid_close(self.interface))
 
     def get_input_report(self, path, size):
@@ -150,7 +188,11 @@ class Interface:
         return _hid_raise("interrupt_write",
                 hid.hid_interrupt_write(self.interface, endpoint, bytes, timeout))
 
-    def set_idle(self, duration, report_id):
+    def set_idle(self, duration=0, report_id):
+        '''Set the idle interval for an interface (report_id=0) or a specific report.
+
+        The duration should be 0 (no reports unless something changes) or a
+        multiple of 4 up to 1020 (milliseconds).'''
         return _hid_raise("set_idle",
                 hid.hid_set_idle(self.interface, duration, report_id))
 
